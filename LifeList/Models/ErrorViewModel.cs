a5f3a65ca7977e9b7d2eb7a77752d74f65e41255
Dana Kiehl <dana.palm87@gmail.com>
2020-07-11 15:50:43 -0500

    initial

---
+++ LifeList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LifeList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
